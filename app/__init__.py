35f8fb76975137e02848b93f4c676d92d3a3b137
Radahika <radhika.marvin@berkeley.edu>
2015-01-16 23:20:16 -0800

    changes to __init__ to hopefully fix gunicorn bug

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, SECRET_KEY
+from flask.ext.mail import Mail
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,32 +14,24 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-if not app.debug:
+mail = Mail(app)
+
+if not app.debug and MAIL_SERVER != '':
     import logging
     from logging.handlers import SMTPHandler
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'pear-soap failure', credentials)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               'microblog failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
-
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('pear-soap startup')
-
-from app import views, models
 
 if not app.debug and os.environ.get('HEROKU') is None:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/pear.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.addHandler(file_handler)
@@ -50,3 +44,6 @@ if os.environ.get('HEROKU') is not None:
     app.logger.addHandler(stream_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('pear-soap startup')
+
+from app import views, models
+
