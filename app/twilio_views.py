9fad7c4fd7d5f2fdea670efd04b94d002eeecd16
Radahika <radhika.marvin@berkeley.edu>
2015-02-06 00:57:10 -0800

    fixed import bug

--- app/twilio_views.py
+++ app/twilio_views.py
@@ -1,6 +1,22 @@
-from app import views, app, client
-from config import TWILIO_NUMBER
+from flask import Flask, render_template, flash, redirect, session, url_for, request, g, jsonify, abort, make_response, request, json
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask.ext.httpauth import HTTPBasicAuth
+from app import app, db, lm, client, views
+from .forms import LoginForm, CreateForm, ResetForm, HouseForm, MessageForm
+from .models import User, Chore, House, Message, Event
+import datetime
+from config import MESSAGES_PER_PAGE, CHORES_PER_PAGE, TWILIO_NUMBER
+import pdb
+
 import twilio
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/twilio/send_message')
 @login_required
