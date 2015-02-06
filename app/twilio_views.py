ca3471beb19304577de4ef2407988c27b84d42a2
Radahika <radhika.marvin@berkeley.edu>
2015-02-06 00:33:46 -0800

    put it in a html call

--- app/twilio_views.py
+++ app/twilio_views.py
@@ -2,17 +2,15 @@ from app import views, app, client
 from config import TWILIO_NUMBER
 import twilio
 
-try:
-    message = client.messages.create(
+@app.route('/twilio/send_message')
+@login_required
+def send_message():
+    user = g.user
+    try:
+        message = client.messages.create(
                 body="Radhika I love you <3",
                 to="+17608213933",
                 from_=TWILIO_NUMBER
     )
-except twilio.TwilioRestException as e:
-    print e
-print message.sid
-
-@app.route('/twilio/send_message')
-def send_message():
-    user = g.user
-    
+    except twilio.TwilioRestException as e:
+        print e
